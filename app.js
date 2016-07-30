3828f31f83649a83d935e289a3ea022cc18a4664
Jamshid <mr.jamshid.a@gmail.com>
2016-07-30 14:10:32 +0430

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react'
+import {View, Text, StyleSheet, Dimensions, ScrollView} from 'react-native'
+const {width:device_width} = Dimensions.get('window');
+
+export default class Main extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            cards: [{title: 'jim', content: 'mohmal'}, {title: 'podcast', content: 'gomaneh'}]
+        }
+    }
+    render(){
+        return(
+            <View>
+                <ScrollView>
+                    {this.state.cards.map((card, i)=> {
+                        return(
+                            <View style={styles.card} key={i}>
+                                <Text>
+                                    {card.title}
+                                </Text>
+                                <Text>
+                                    {card.content}
+                                </Text>
+                            </View>
+                        )
+                    })}
+                </ScrollView>
+            </View>
+        )
+    }
+}
+
+var styles = StyleSheet.create({
+    card: {
+        borderRadius: 5,
+        backgroundColor: '#ffffff',
+        height: 100,
+        width: device_width - 20,
+        marginHorizontal: 10,
+        marginVertical: 10,
+        padding: 10,
+        
+    }
+});
---
+++ index.android.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import Main from './app'
+class ToDo extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Main />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'grey',
+  }
+});
+
+AppRegistry.registerComponent('ToDo', () => ToDo);
